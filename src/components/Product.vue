8bd13c2d87ce626798e800062e7a425e832101ca
vagaganova <69433218+vagaganova@users.noreply.github.com>
2022-02-14 23:48:41 +0300

    Add files via upload

---
+++ src/components/Product.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="card" v-on:click="$emit('add-to-card',product.id)">
+        <h2>{{product && product.title || ''}}</h2>
+        <p>{{product && product.price || ''}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Product",
+        props:{
+            /**
+             * {
+             *     id: string | number
+             *     title: string
+             *     price: number
+             * }
+             */
+            product: Object
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .card{
+        flex-basis: 20%;
+    }
+</style>
